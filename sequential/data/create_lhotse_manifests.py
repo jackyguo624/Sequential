e9e1c8aac811ae02e460907158f9bdb9aaa40125
Jiaqi Guo <leon.k.scott@gmail.com>
2025-04-24 09:51:44 +0800

    [Update] data prepare recipe

--- sequential/data/create_lhotse_manifests.py
+++ sequential/data/create_lhotse_manifests.py
@@ -1,49 +1,89 @@
-from lhotse import RecordingSet, SupervisionSet, CutSet
+import kaldiio
 import json
+import tqdm
+import argparse
+from lhotse import (
+    Recording, AudioSource, SupervisionSegment, RecordingSet, SupervisionSet
+)
+from multiprocessing import Pool
+from functools import partial
+import logging
+'''
+aisv1 format:
+{
+    "key": "xxx",
+    "task": "ASR",
+    "target": "text",
+    "path": "kaldi.ark:offset",
+}
+'''
 
-def convert_jsonl_to_lhotse(jsonl_path, output_dir):
+def process_aisv1_jsonl(line, num_channels: int =1):
+    data = json.loads(line)
+    # 使用kaldiio读取mat文件, read wav.ark:offset to (sample_rate, narray1d)
+    sampling_rate, samples = kaldiio.load_mat(data['path'])
+    num_samples = samples.shape[0] 
+    channels = [i for i in range(num_channels)]
+    duration = num_samples / num_channels / sampling_rate
+    # 创建Recording
+    recording = Recording(
+        id=data['key'],
+        sources=[
+            AudioSource(type='file',
+                        channels=channels,
+                        source=data['path'])
+        ],
+        sampling_rate=sampling_rate,
+        duration=duration,
+        num_samples=num_samples
+    )
+
+    # 创建Supervision
+    supervision = SupervisionSegment(
+        id=data['key'],
+        recording_id=data['key'],
+        start=0.0,
+        duration=duration,
+        channel=channels,
+        text=data['target'],
+    )
+    return recording, supervision
+
+def convert_aisv1_to_manifest(jsonl_path, output_dir, num_channels=1, num_workers=1):
     recordings = []
     supervisions = []
-    
-    with open(jsonl_path) as f:
-        for line in f:
-            data = json.loads(line)
-            
-            # 创建Recording
-            recordings.append({
-                'type': 'file',
-                'path': data['audio_path'],
-                'duration': data['duration'],
-                'sampling_rate': 16000  # 根据实际采样率修改
-            })
-            
-            # 创建Supervision
-            supervisions.append({
-                'recording_id': data['audio_path'],  # 用路径作为唯一ID
-                'start': 0.0,
-                'duration': data['duration'],
-                'text': data['text'],
-                'language': data['language'],
-                # 可添加其他自定义字段
-                'custom': {k: v for k, v in data.items() if k not in ['audio_path', 'text', 'duration']}
-            })
-    
-    # 转换为Lhotse对象
-    recording_set = RecordingSet.from_jsonl_lazy(recordings)
-    supervision_set = SupervisionSet.from_dicts(supervisions)
-    
-    # 创建CutSet
-    cut_set = CutSet.from_manifests(
-        recordings=recording_set,
-        supervisions=supervision_set
-    )
-    
-    # 保存结果
-    recording_set.to_file(f"{output_dir}/recordings.jsonl")
-    cut_set.to_file(f"{output_dir}/cuts.jsonl")
 
-# 使用示例
-convert_jsonl_to_lhotse(
-    jsonl_path="/aistor/aispeech/hpc_stor01/group/asr/collection/zh+en+fangyan-2w-asr/train/multitask.jsonl",
-    output_dir="./lhotse_data"
-) 
+    with open(jsonl_path, 'r') as f:
+        lines = f.readlines()
+        func = partial(process_aisv1_jsonl, num_channels=num_channels)
+        with Pool(num_workers) as p:
+            for recording, supervision in tqdm.tqdm(p.imap(func, lines), total=len(lines)):
+                recordings.append(recording)
+                supervisions.append(supervision)
+
+    # 转换为Lhotse对象
+    recording_set = RecordingSet.from_recordings(recordings)
+    supervision_set = SupervisionSet.from_segments(supervisions)
+
+    # 保存结果
+    logging.info(f"Saving recordings to {output_dir}/recordings.jsonl")
+    recording_set.to_file(f"{output_dir}/recordings.jsonl")
+    logging.info(f"Saving supervisions to {output_dir}/supervisions.jsonl")
+    supervision_set.to_file(f"{output_dir}/supervisions.jsonl")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--jsonl_path", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--num-channels", type=int, default=1)
+    parser.add_argument("--format", type=str, default="aisv1", choices=["aisv1"])
+    parser.add_argument("--num-workers", type=int, default=1)
+
+    args = parser.parse_args()
+    func = f"convert_{args.format}_to_manifest"
+    globals()[func](
+        jsonl_path=args.jsonl_path,
+        output_dir=args.output_dir,
+        num_channels=args.num_channels,
+        num_workers=args.num_workers
+    )
---
+++ sequential/data/lhotse_datamodule.py
@@ -0,0 +1,133 @@
+import pytorch_lightning as pl
+from typing import Optional, Callable, Dict
+from lhotse import CutSet
+from lhotse import RecordingSet, SupervisionSet
+from lhotse.dataset.sampling.base import CutSampler
+from lhotse.dataset.dataloading import make_worker_init_fn
+from torch.utils.data import DataLoader, Dataset
+from pathlib import Path
+from typing import Tuple, Union
+import os
+
+
+class LhotseDataModule(pl.LightningDataModule):
+    def __init__(self,
+                 train_dataset: Callable[[], Dataset],
+                 train_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler],
+                 val_dataset: Callable[[], Dataset] = None,
+                 val_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 test_dataset: Callable[[], Dataset] = None,
+                 test_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 ):
+        super().__init__()
+        self.train_dataset = train_dataset
+        self.train_sampler = train_sampler
+        self.val_dataset = val_dataset
+        self.val_sampler = val_sampler
+        self.test_dataset = test_dataset
+        self.test_sampler = test_sampler
+    
+    def get_worker_init_fn(self) -> Callable | None:
+        num_node = 1
+        if os.environ.get("NODE_RANK", None) is not None:
+            rank = int(os.environ.get("NODE_RANK", None)) * num_node + \
+                int(os.environ.get("LOCAL_RANK", None))
+            world_size = int(os.environ.get("WORLD_SIZE", None))
+            worker_init_fn = make_worker_init_fn(rank=rank, world_size=world_size)
+            print(f'rank: {rank}, world: {world_size} ')
+        else:
+            worker_init_fn = None
+        return worker_init_fn
+
+    def get_dataloader(self, dataset: Callable[[], Dataset],
+                       sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler],
+                       cuts: CutSet):
+
+        batch_sampler = sampler(cuts)
+        worker_init_fn = self.get_worker_init_fn()
+        print(batch_sampler)
+        dataloader = DataLoader(dataset(),
+                                batch_sampler=batch_sampler,
+                                num_workers=10,
+                                persistent_workers=True,
+                                worker_init_fn=worker_init_fn,
+                                collate_fn=lambda x: x)
+
+        return dataloader
+
+
+class LhotseRecipesDataModule(LhotseDataModule):
+    def __init__(self,
+                 train_dataset: Callable[[], Dataset],
+                 train_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler],
+                 val_dataset: Callable[[], Dataset] = None,
+                 val_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 test_dataset: Callable[[], Dataset] = None,
+                 test_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 train_manifest: str = None,
+                 val_manifest: str = None,
+                 test_manifest: str = None,
+                 download: Callable = None,
+                 prepare: Callable = None,
+                 ):
+        super().__init__(train_dataset, train_sampler, val_dataset, val_sampler, test_dataset, test_sampler)
+        self.download = download
+        self.prepare = prepare
+        self.train_manifest = train_manifest
+        self.val_manifest = val_manifest
+        self.test_manifest = test_manifest
+
+
+    def setup(self, stage: Optional[str] = None):
+        self.manifests = self.prepare
+
+    def train_dataloader(self):
+        manifests = self.manifests
+        if 'cuts' in manifests:
+            cuts = manifests['cuts']
+        else:
+            cuts = CutSet.from_manifests(recordings=manifests['recordings'], 
+                                         supervisions=manifests['supervisions'])
+        return self.get_dataloader(self.train_dataset, self.train_sampler, cuts)
+
+
+
+class LhotseManifestDataModule(LhotseDataModule):
+    def __init__(self,
+                 train_manifest_dir: Path,
+                 train_dataset: Callable[[], Dataset],
+                 train_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler],
+                 val_manifest_dir: Path = None,
+                 val_dataset: Callable[[], Dataset] = None,
+                 val_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 test_manifest_dir: Path = None,
+                 test_dataset: Callable[[], Dataset] = None,
+                 test_sampler: Callable[[Union[CutSet, Tuple[CutSet]]], CutSampler] = None,
+                 ):
+        super().__init__(train_dataset, train_sampler, val_dataset, val_sampler, test_dataset, test_sampler)
+        self.train_manifest_dir = train_manifest_dir
+        self.val_manifest_dir = val_manifest_dir
+        self.test_manifest_dir = test_manifest_dir
+    
+    def load_manifest(self, manifest_dir: Path) -> Dict:
+        recording_set = RecordingSet.from_jsonl_lazy(manifest_dir / "recordings.jsonl")
+        supervision_set = SupervisionSet.from_jsonl_lazy(manifest_dir / "supervisions.jsonl")
+        cut_set = CutSet.from_manifests(recordings=recording_set, supervisions=supervision_set)
+        return {"recordings": recording_set, "supervisions": supervision_set, "cuts": cut_set}
+    
+    def setup(self, stage: Optional[str] = None):
+        if self.train_manifest_dir and stage == "fit":
+            self.train_manifest = self.load_manifest(self.train_manifest_dir)
+        if self.val_manifest_dir and stage == "fit":
+            self.val_manifest = self.load_manifest(self.val_manifest_dir)
+        if self.test_manifest_dir and stage == "test":
+            self.test_manifest = self.load_manifest(self.test_manifest_dir)
+    
+    def train_dataloader(self):
+        if self.train_manifest: 
+            cuts = self.train_manifest['cuts']
+        else:
+            cuts = CutSet.from_manifests(recordings=self.train_manifest['recordings'], 
+                                         supervisions=self.train_manifest['supervisions'])
+        return self.get_dataloader(self.train_dataset, self.train_sampler, cuts)
+    
